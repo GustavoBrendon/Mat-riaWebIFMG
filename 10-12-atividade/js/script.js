62ce7c16dc2c2122967d411a08da9f48d51b04c2
Gustavo Brendon Gomes Pimenta <0070560@academico.ifmg.edu.br>
2024-12-10 10:18:22 -0300

    atividade08 10-12

---
+++ 10-12-atividade/js/script.js
@@ -0,0 +1,19 @@
+import troca from "./functions.js";
+  
+const cartaCostas1 = document.getElementById('carta1');
+  cartaCostas1.addEventListener('click', troca);
+  
+const cartaCostas2 = document.getElementById('carta2');
+cartaCostas2.addEventListener('click', troca);
+
+const cartaCostas3 = document.getElementById('carta3');
+cartaCostas3.addEventListener('click', troca);
+
+const cartaCostas4 = document.getElementById('carta4');
+cartaCostas4.addEventListener('click', troca);
+
+const cartaCostas5 = document.getElementById('carta5');
+cartaCostas5.addEventListener('click', troca);
+
+const cartaCostas6 = document.getElementById('carta6');
+cartaCostas6.addEventListener('click', troca);
