4eb45ceeb540feeb328f05a36e6eafcd6fa0f4ca
GustavoBrendon <guhgomesyt@gmail.com>
2024-12-10 15:19:04 -0300

    finalizando atividade

--- 10-12-atividade/js/script.js
+++ 10-12-atividade/js/script.js
@@ -1,19 +1,22 @@
-import troca from "./functions.js";
-  
-const cartaCostas1 = document.getElementById('carta1');
-  cartaCostas1.addEventListener('click', troca);
-  
-const cartaCostas2 = document.getElementById('carta2');
-cartaCostas2.addEventListener('click', troca);
+const cartasSection1 = document.querySelectorAll('#section1 img');
+const cartasSection2 = document.querySelectorAll('#section2 img');
 
-const cartaCostas3 = document.getElementById('carta3');
-cartaCostas3.addEventListener('click', troca);
+cartasSection2.forEach((carta) => {
+  carta.addEventListener('click', () => {
+    const cartaDisponivel = Array.from(cartasSection1).find((carta) => 
+      carta.src.includes('Carta-Costas.png')
+    );
+    if (cartaDisponivel) {
+      cartaDisponivel.src = carta.src;
+    } else {
+      alert('Não há mais espaço disponível!');
+    }
+  });
+});
 
-const cartaCostas4 = document.getElementById('carta4');
-cartaCostas4.addEventListener('click', troca);
 
-const cartaCostas5 = document.getElementById('carta5');
-cartaCostas5.addEventListener('click', troca);
-
-const cartaCostas6 = document.getElementById('carta6');
-cartaCostas6.addEventListener('click', troca);
+cartasSection1.forEach((carta) => {
+  carta.addEventListener('click', () => {
+    carta.src = './img/Carta-Costas.png';
+  });
+});
